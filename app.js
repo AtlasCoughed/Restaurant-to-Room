f779ea35afa859f05d39b0f2b54d752595e101e3
Mike Tran <michaelhuytran@gmail.com>
2016-08-12 01:17:24 +0000

    [Features] Added Authprotocols

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 var express = require('express');
 var path = require('path');
- var logger = require('morgan');
+var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var expressSession = require('express-session');
+var flash = require('connect-flash');
+var connectMongo = require('connect-mongo');
 
+var config = require('./config');
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var orders = require('./routes/orders');
+
+var MongoStore = connectMongo(expressSession);
+
+var passportConfig = require('./auth/passport-config');
+var restrict = require('./auth/restrict');
+passportConfig();
+
+mongoose.connect(config.mongoUri);
 
 var app = express();
 
@@ -13,27 +28,39 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(expressSession(
+  {
+    secret : 'getting hungry',
+    saveUninitialized: false,
+    resave: false,
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection
+    })
+  }
+));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', routes);
 app.use('/users', users);
+app.use('/orders', orders);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
-});
+}); 
 
 // error handlers
 
-// development error handler
-// will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
     res.status(err.status || 500);
